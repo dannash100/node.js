704b83519c012edca543b5d2f19cf6c57fdcd25a
dannash100 <danster15@gmail.com>
2018-11-05 18:35:56 +1300

    listenerCount

--- reflection.js
+++ reflection.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-console */
 const events = require('events');
+
 
 class EventTracker extends events.EventEmitter {}
 
 const eventTracker = new EventTracker();
 
-eventTracker.on('newListener', (name, listener) => {
+eventTracker.on('newListener', (name) => {
   console.log('Event name added:', name);
 });
 
@@ -12,14 +14,14 @@ eventTracker.on('a listener', () => {
   // causes 'newListener' event to fire
 });
 
-// trigger events on 'newListener' event
+// following example shows how to trigger events on 'newListener'.
 
 class Pulsar extends events.EventEmitter {
   constructor(speed, times) {
     super();
     this.speed = speed;
     this.times = times;
-    this.on('newListener', (eventName, listener) => {
+    this.on('newListener', (eventName) => {
       if (eventName === 'pulse') this.start();
     });
   }
@@ -27,11 +29,17 @@ class Pulsar extends events.EventEmitter {
   start() {
     const id = setInterval(() => {
       this.emit('pulse');
-      this.times--;
+      this.times = this.times - 1;
       if (this.times === 0) {
         clearInterval(id);
       }
     }, this.speed);
+  }
+
+  stop() {
+    if (this.listenerCount('pulse') === 0) {
+      throw new Error('no listeners have been added');
+    }
   }
 }
 
