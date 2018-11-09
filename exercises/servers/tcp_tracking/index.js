982b3289fc4fb333e43ce2fecad973140d8dc7ca
dannash100 <danster15@gmail.com>
2018-11-10 11:52:47 +1300

    tcp test

--- exercises/servers/tcp_tracking/index.js
+++ exercises/servers/tcp_tracking/index.js
@@ -1,9 +1,14 @@
+/* eslint-disable max-len */
 /* eslint-disable no-console */
 const net = require('net');
+const assert = require('assert');
 
 let clients = 0;
+let expectedAssertions = 2;
 
-// $ telnet localhost 8000
+// node index - $ telnet localhost 8000
+// net connect is used to connect to server by port number, returns a EventEmmiter to listen to events
+
 
 const server = net.createServer((client) => {
   clients += 1;
@@ -14,10 +19,29 @@ const server = net.createServer((client) => {
     console.log('client disconnected', clientId);
   });
 
-  client.write(`welcome client: ${clientId}rn`);
+  client.write(`welcome client: ${clientId}\r\n`);
   client.pipe(client);
 });
 
+
+const runTest = (expectedId, done) => {
+  const client = net.connect(8000);
+
+  client.on('data', (data) => {
+    const expected = `welcome client: ${expectedId}\r\n`;
+    assert.equal(data.toString(), expected);
+    expectedAssertions -= 1;
+    client.end();
+  });
+  client.on('end', done);
+};
+
 server.listen(8000, () => {
   console.log('Server started on port 8000');
+  runTest(1, () => {
+    runTest(2, () => {
+      assert.equal(0, expectedAssertions);
+      server.close();
+    });
+  });
 });
