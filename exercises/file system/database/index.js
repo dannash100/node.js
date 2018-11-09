b84998676454a8a6efbb4f395d29aeff78d06af6
dannash100 <danster15@gmail.com>
2018-11-09 15:29:22 +1300

    exercises

---
+++ exercises/file system/database/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/* eslint-disable consistent-return */
+
+const Database = require('./database');
+
+const client = new Database('./test.db');
+
+client.on('load', () => {
+  const foo = client.get('foo');
+
+  client.set('bar', 'my sweet value', (err) => {
+    if (err) return console.error(err);
+    console.log('write succesful');
+  });
+
+  client.del('baz');
+});
