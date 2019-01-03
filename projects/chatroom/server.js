dc818b310e9cb683637c8aeaecac0b08fc6185b9
dannash100 <danster15@gmail.com>
2019-01-03 16:44:12 +1300

    chatroom project server

---
+++ projects/chatroom/server.js
@@ -0,0 +1,55 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const mime = require('mime-types');
+
+const cache = {};
+
+const send404 = (response) => {
+  response.writeHead(404, {
+    'Content-Type': 'text/plain',
+  });
+  response.write('Error 404: resource not found.');
+  response.end();
+};
+
+const sendFile = (response, filePath, fileContents) => {
+  response.writeHead(200, {
+    'content-type': mime.lookup(path.basename(filePath)),
+  });
+  response.end(fileContents);
+};
+
+const serveStatic = (response, cache, absPath) => {
+  if (cache[absPath]) {
+    sendFile(response, absPath, cache[absPath]);
+  } else {
+    fs.exists(absPath, (exists) => {
+      if (exists) {
+        fs.readFile(absPath, (err, data) => {
+          if (err) {
+            send404(response);
+          } else {
+            cache[absPath] = data;
+            sendFile(response, absPath, data);
+          }
+        });
+      } else {
+        send404(response);
+      }
+    });
+  }
+};
+
+const server = http.createServer((request, response) => {
+  let filePath = false;
+  if (request.url === '/') {
+    filePath = 'public/index.html';
+  } else {
+    filePath = `public${request.url}`;
+  }
+  const absPath = `./${filePath}`;
+  serveStatic(response, cache, absPath);
+});
+
+server.listen(3000, () => console.log('server listening on port 3000.'));
